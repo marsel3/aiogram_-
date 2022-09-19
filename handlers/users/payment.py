1e6ea0091e77cd58e7ba3887e864b57107f72e81
marcel3 <akbulatov03@mail.ru>
2022-09-19 22:20:18 +0300

    In process...

--- handlers/users/payment.py
+++ handlers/users/payment.py
@@ -5,21 +5,16 @@ from aiogram.types import CallbackQuery
 from data.config import PAYMENT_TOKEN
 from loader import dp, db_users
 
-PRICES = [
-    LabeledPrice(label='Ноутбук', amount=1000),
-    LabeledPrice(label='Прочная упаковка', amount=1000)
-]
 
 SUPERSPEED_SHIPPING_OPTION = ShippingOption(
     id='superspeed',
-    title='Супер быстрая!'
+    title='Доставка по городу'
 ).add(LabeledPrice('Лично в руки!', 1000))
 
 POST_SHIPPING_OPTION = ShippingOption(
     id='post',
     title='Почта России'
 )
-
 POST_SHIPPING_OPTION.add(LabeledPrice('Кортонная коробка', 1000))
 POST_SHIPPING_OPTION.add(LabeledPrice('Срочное отправление!', 1000))
 
@@ -27,29 +22,26 @@ PICKUP_SHIPPING_OPTION = ShippingOption(
     id='pickup',
     title='Самовывоз'
 )
-PICKUP_SHIPPING_OPTION.add(LabeledPrice('Самовывоз в Сантк-Петербурге', 1000))
+PICKUP_SHIPPING_OPTION.add(LabeledPrice('Самовывоз в Казани', 1000))
 
 
 @dp.callback_query_handler(text='pay')
 async def buy_process(call: CallbackQuery):
     PAYMENTS = []
     for i in db_users.basket_list(call.from_user.id):
-        print(i)
-        print(i[2] * i[3] * 100)
-        PAYMENTS.append(LabeledPrice(label=i[1], amount=i[2] * i[3] * 100))
+        PAYMENTS.append(LabeledPrice(label=i[1], amount=int(i[2] * i[3] * 100)))
 
     await dp.bot.delete_message(call.message.chat.id, call.message.message_id)
     await dp.bot.send_invoice(
             chat_id=call.message.chat.id,
             title='Заказ №190922',
             description='самовывоз',
-            invoice_payload='true',
-            provider_token=PAYMENT_TOKEN,
             currency='RUB',
+            provider_token=PAYMENT_TOKEN,
             need_email=True,
             need_phone_number=True,
             is_flexible=True,
-            prices=PRICES,
+            prices=PAYMENTS,
             start_parameter='example',
             payload='some_invoice')
 
@@ -63,15 +55,15 @@ async def shipping_process(shipping_query: ShippingQuery):
             error_message='AU_error'
         )
 
-    shipping_options = [SUPERSPEED_SHIPPING_OPTION]
+    shipping_options = []
 
-    if shipping_query.shipping_address.city == 'Казань':
+    if shipping_query.shipping_address.city.lower() == 'казань':
         shipping_options.append(PICKUP_SHIPPING_OPTION)
+        shipping_options.append(SUPERSPEED_SHIPPING_OPTION)
     elif shipping_query.shipping_address.country_code == 'RU':
         shipping_options.append(POST_SHIPPING_OPTION)
 
-        if shipping_query.shipping_address.city == 'Санкт-Петербург':
-            shipping_options.append(PICKUP_SHIPPING_OPTION)
+
 
     await dp.bot.answer_shipping_query(
         shipping_query.id,
