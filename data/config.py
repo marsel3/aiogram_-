76752884fa35a92f83869505d7d28326829d6040
marcel3 <akbulatov03@mail.ru>
2023-08-26 14:51:57 +0200

    In process...

--- data/config.py
+++ data/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 #BOT_TOKEN = str(os.getenv(("BOT_TOKEN")))
 BOT_TOKEN = '1672438859:AAEVf_aKog8XvhYrERzcXdQ7WljolArQ7sc'
-PAYMENT_TOKEN = str(os.getenv(("PAYMENT_TOKEN")))
+PAYMENT_TOKEN = "381764678:TEST:65037"
 
 #PGUSER = str(os.getenv(("PGUSER")))
 #PGPASSWORD = str(os.getenv(("PGPASSWORD")))
--- handlers/users/payment.py
+++ handlers/users/payment.py
@@ -2,39 +2,32 @@ from aiogram.types import Message, ShippingOption, ShippingQuery, LabeledPrice, 
 from aiogram.types.message import ContentType
 from aiogram.types import CallbackQuery
 from data.config import admins_id
-
+from utils.db_api.db_asyncpg import *
 from data.config import PAYMENT_TOKEN
 from loader import dp
 
 
-SUPERSPEED_SHIPPING_OPTION = ShippingOption(
-    id='city_delivery',
-    title='Доставка по городу!'
-).add(LabeledPrice('Лично в руки!', 20000))
+SUPERSPEED_SHIPPING_OPTION = ShippingOption(id='city_delivery',
+                                            title='Доставка по городу!').add(LabeledPrice('Лично в руки!', 20000))
 
-POST_SHIPPING_OPTION = ShippingOption(
-    id='post',
-    title='Почта России'
-)
+POST_SHIPPING_OPTION = ShippingOption(id='post', title='Почта России')
+
 POST_SHIPPING_OPTION.add(LabeledPrice('Картонная коробка', 1700))
 POST_SHIPPING_OPTION.add(LabeledPrice('Срочное отправление!', 2500))
 POST_SHIPPING_OPTION.add(LabeledPrice('Кортонная коробка', 1000))
 POST_SHIPPING_OPTION.add(LabeledPrice('Срочное отправление!', 1000))
 
-PICKUP_SHIPPING_OPTION = ShippingOption(
-    id='pickup',
-    title='Самовывоз'
-)
+PICKUP_SHIPPING_OPTION = ShippingOption(id='pickup', title='Самовывоз')
 PICKUP_SHIPPING_OPTION.add(LabeledPrice('Самовывоз в Казани', 1000))
 
 
 @dp.callback_query_handler(text='pay')
 async def buy_process(call: CallbackQuery):
     PRICES = []
-    for i in db_users.basket_list(call.from_user.id):
-        PRICES.append(LabeledPrice(i[1], int(i[2] * i[3] * 100)))
+    tovars = await basket_list(call.from_user.id)
+    for tovar in tovars:
+        PRICES.append(LabeledPrice(tovar["name"], int(tovar["price"] * tovar["count"] * 100)))
     await dp.bot.delete_message(call.message.chat.id, call.message.message_id)
-    #try:
     await dp.bot.send_invoice(
         chat_id=call.message.chat.id,
         title='Заказ № ???',
@@ -47,32 +40,30 @@ async def buy_process(call: CallbackQuery):
         prices=PRICES,
         start_parameter='example',
         payload='some_invoice')
-    #except:
-        #await dp.bot.send_message(chat_id = call.message.chat.id,
-                                  #text='Минимальная сумма заказа 1$')
 
 
 @dp.shipping_query_handler(lambda q: True)
 async def shipping_process(shipping_query: ShippingQuery):
-    if shipping_query.shipping_address.country_code == 'AU':
+    shipping_options = []
+    if shipping_query.shipping_address.country_code != 'RU':
         return await dp.bot.answer_shipping_query(
             shipping_query.id,
             ok=False,
-            error_message='AU_error'
+            error_message='Измените страну'
         )
+    else:
+        if shipping_query.shipping_address.city.title() == 'Казань':
+            shipping_options.append(PICKUP_SHIPPING_OPTION)
+            shipping_options.append(SUPERSPEED_SHIPPING_OPTION)
 
-    shipping_options = []
-    if shipping_query.shipping_address.city.title() == 'Казань':
-        shipping_options.append(PICKUP_SHIPPING_OPTION)
-        shipping_options.append(SUPERSPEED_SHIPPING_OPTION)
-    elif shipping_query.shipping_address.country_code == 'RU':
-        shipping_options.append(POST_SHIPPING_OPTION)
+        else:
+            shipping_options.append(POST_SHIPPING_OPTION)
 
-    await dp.bot.answer_shipping_query(
-        shipping_query.id,
-        ok=True,
-        shipping_options=shipping_options
-    )
+        await dp.bot.answer_shipping_query(
+            shipping_query.id,
+            ok=True,
+            shipping_options=shipping_options
+        )
 
 
 @dp.pre_checkout_query_handler(lambda q: True)
@@ -87,7 +78,7 @@ async def successful_payment(message: Message):
         'Заказ оплачен, и уже начал собираться)\nСкоро с вами свяжется наш менеджер!'
     )
     print(message.successful_payment)
-    user_name = db_users.user_fullname(message.from_user.id)
+    user_name = message.from_user.full_name
 
     pmnt = message.successful_payment.to_python()
     for key, val in pmnt.items():
