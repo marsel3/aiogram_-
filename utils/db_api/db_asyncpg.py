8f4604b4a0a956b37b49b4098a4fee030f3b5fbe
marcel3 <akbulatov03@mail.ru>
2023-08-27 18:30:27 +0200

    In process...

--- utils/db_api/db_asyncpg.py
+++ utils/db_api/db_asyncpg.py
@@ -11,7 +11,7 @@ async def user_exists(user_id: int):
 async def add_user(user_id: int, fio: str, referral=None):
     async with dp['db_pool'].acquire() as connection:
         async with connection.transaction():
-            await connection.execute('INSERT INTO "users" ("user_id", "fio", "referralz") '
+            await connection.execute('INSERT INTO "users" ("user_id", "fio", "referral") '
                                      'VALUES ($1, $2, $3)', user_id, fio, referral)
 
 
