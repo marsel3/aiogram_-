7e80db6e2efc0eff1ff733288034694c109a8d0a
marssak <akbulatov-marsel@mail.ru>
2023-08-31 10:21:50 +0300

    my

--- handlers/users/main.py
+++ handlers/users/main.py
@@ -1,14 +1,19 @@
 from aiogram import types
-from loader import dp
-from keyboards.default import keyboard_menu
-from utils.db_api.db_asyncpg import *
 
+from loader import dp, db_users
+from keyboards.default import keyboard_menu
+from handlers.users.commands import start
 
 @dp.message_handler()
 async def main(message: types.Message):
     exists = await user_exists(message.from_user.id)
     if not exists:
         referral = None
+        unique_code = extract_unique_code(message.text)
+        if unique_code and await user_exists(unique_code):
+            referral = unique_code
+
+        print(referral)
         await add_user(user_id=message.from_user.id,
                        fio=message.from_user.full_name,
                        referral=referral)
@@ -17,3 +22,4 @@ async def main(message: types.Message):
                          f'\nНажимай на каталог и начинай собирать заказ 😉',
                          reply_markup=keyboard_menu.main)
     await message.delete()
+
